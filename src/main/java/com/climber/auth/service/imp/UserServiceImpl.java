7049d74cc8510c292c73453c734501db4720d618
derlinchen <derlin_nj@163.com>
2019-01-21 19:50:34 +0800

    update

--- src/main/java/com/climber/auth/service/imp/UserServiceImpl.java
+++ src/main/java/com/climber/auth/service/imp/UserServiceImpl.java
@@ -2,12 +2,12 @@ package com.climber.auth.service.imp;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.climber.auth.bean.UserInfo;
 import com.climber.auth.bean.UserInfoDetails;
 import com.climber.auth.service.UserService;
-import com.climber.auth.utils.MD5Utils;
 
 
 @Service("userService")
@@ -17,8 +17,8 @@ public class UserServiceImpl implements UserService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserDetails rtn = null;
 		
-//		UserInfo user = new UserInfo(1, "user1", new BCryptPasswordEncoder().encode("123456789"));
-		UserInfo user = new UserInfo(1, "user1", "{MD5}"+MD5Utils.encode("123456789"));
+		UserInfo user = new UserInfo(1, "user1", "{bcrypt}"+new BCryptPasswordEncoder().encode("123456789"));
+//		UserInfo user = new UserInfo(1, "user1", "{MD5}"+MD5Utils.encode("123456789"));
 		if(user.getUserName().equals(username)){
 			rtn = UserDetailConverter.convert(user);
 		}
